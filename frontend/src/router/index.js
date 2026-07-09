c6ee5c9e28496e83c795f338082b357673582a5c
Abhishek <23f3004157@ds.study.iitm.ac.in>
2026-07-10 00:47:47 +0530

    Completed frontend and implemented route guards

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -113,4 +113,93 @@ const router = createRouter({
 
 });
 
+
+router.beforeEach((to, from, next) => {
+
+    const token = localStorage.getItem("token")
+
+    const role = localStorage.getItem("role")
+
+    if (
+        to.path === "/" ||
+        to.path === "/register"
+    ) {
+
+        if (!token) {
+
+            return next()
+
+        }
+
+        if (role === "admin") {
+
+            return next("/admin")
+
+        }
+
+        if (role === "staff") {
+
+            return next("/staff")
+
+        }
+
+        return next("/trekker")
+
+    }
+
+    if (!token) {
+
+        return next("/")
+
+    }
+
+    if (
+        to.path.startsWith("/admin") &&
+        role !== "admin"
+    ) {
+
+        if (role === "staff") {
+
+            return next("/staff")
+
+        }
+
+        return next("/trekker")
+
+    }
+
+    if (
+        to.path.startsWith("/staff") &&
+        role !== "staff"
+    ) {
+
+        if (role === "admin") {
+
+            return next("/admin")
+
+        }
+
+        return next("/trekker")
+
+    }
+
+    if (
+        to.path.startsWith("/trekker") &&
+        role !== "trekker"
+    ) {
+
+        if (role === "admin") {
+
+            return next("/admin")
+
+        }
+
+        return next("/staff")
+
+    }
+
+    next()
+
+})
+
 export default router;
