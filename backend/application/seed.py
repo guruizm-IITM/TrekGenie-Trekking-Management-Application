747168e46e7f37f141632957635fad89fa459a1a
Abhishek Guru <23f3004157@ds.study.iitm.ac.in>
2026-07-15 19:06:43 +0530

    Updated seed.py and gitignore

--- backend/application/seed.py
+++ backend/application/seed.py
@@ -5,14 +5,14 @@ from application.models import User
 def create_admin():
 
     admin = User.query.filter_by(
-        email="admin@trek.com"
+        email="admin@trekgenie.com"
     ).first()
 
     if admin is None:
 
         admin = User(
             name="Administrator",
-            email="admin@trek.com",
+            email="admin@trekgenie.com",
             role="admin",
             phone="9999999999"
         )
